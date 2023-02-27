3ea4bb2f07242a23cb46d24b8f5b188593938bbc
Nefelin <flcarmonac@yahoo.com>
2023-02-27 16:43:05 -0300

    Update

---
+++ componentes/Scroll_Snap.js
@@ -0,0 +1,7 @@
+export function Scroll_Snap() {
+    const btn = document.createElement("button");
+    btn.innerText = "Prueba"
+    btn.value = 1;
+
+    return btn;
+}
--- js/index.js
+++ js/index.js
@@ -1,8 +1,30 @@
+import { Scroll_Snap } from "../componentes/Scroll_Snap.js";
 
-function newNavBar01() {
-    $("#demo-contenedor").load("../componentes/navbar_01.html #navBar01");
+window.onload = () => {
+    inicializarBtns();
 }
 
-function newNavBarParalax() {
-    $("#demo-contenedor").load("../componentes/navbar_01.html #navBar01Paralax");
+function inicializarBtns() {
+    const btns = document.querySelectorAll(".btnSection > button");
+
+    btns.forEach(e => {
+        e.addEventListener("click", () => {
+            renderElement(e.value);
+        });
+    });
+};
+
+function renderElement(id) {
+    const demoContenedor = document.querySelector("#demo-contenedor");
+    demoContenedor.innerHTML = "";
+
+    id == 3 ? demoContenedor.appendChild(Scroll_Snap()) : false;
 }
+
+// function newNavBar01() {
+//     $("#demo-contenedor").load("../componentes/navbar_01.html #navBar01");
+// }
+
+// function newNavBarParalax() {
+//     $("#demo-contenedor").load("../componentes/navbar_01.html #navBar01Paralax");
+// }
