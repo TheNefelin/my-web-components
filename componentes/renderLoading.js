cd3fdb1b4cd25b94cb071aecf942c4ab8fdcdc37
TheNefelin <metalflap@gmail.com>
2023-03-11 23:45:46 -0300

    Update

--- componentes/renderLoading.js
+++ componentes/renderLoading.js
@@ -1,9 +1,9 @@
 export default function renderLoading() {
     const contenedor = document.createElement("section");
-    contenedor.classList.add("loading2-contenedor");
+    contenedor.classList.add("loading-contenedor");
 
     const div = document.createElement("div");
-    div.classList.add("loading2");
+    div.classList.add("loading");
 
     for (let i = 1; i <= 20; i++) {
         const span = document.createElement("span");
@@ -22,10 +22,10 @@ export function renderLoadin2() {
     demoContenedor.innerHTML = "";
 
     const contenedor = document.createElement("section");
-    contenedor.classList.add("loading2-contenedor");
+    contenedor.classList.add("loading-contenedor");
 
     const div = document.createElement("div");
-    div.classList.add("loading2");
+    div.classList.add("loading");
 
     for (let i = 1; i <= 20; i++) {
         const span = document.createElement("span");
